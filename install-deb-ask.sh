83e1427b3eddfd4ffc365ef36070e6a2f8b80bad
calllivecn <c-all@qq.com>
2019-02-20 21:39:58 +0800

    修改：install-deb-ask.sh

--- install-deb-ask.sh
+++ install-deb-ask.sh
@@ -17,5 +17,20 @@ done
 }
 
 
-install_for_file "python第3方包.txt"
 install_for_file "ubuntu软件包列表.txt"
+
+pip_install_for_file(){
+for deb in $(grep -Ev '^#|^$' "$1")
+do
+	echo "安装$deb?[y/N]"
+	read -n 1 yesno
+	echo ''
+	if [ "$yesno"x = "y"x ];then
+		sudo pip3 install "$deb"
+	else
+		echo "不安装$deb"
+	fi
+done
+}
+
+pip_install_for_file "python第3方包.txt"
