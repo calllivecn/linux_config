06527d72508870b862e53dcc0978d1fe0365cd42
calllivecn <c-all@qq.com>
2019-04-05 21:38:38 +0800

    修改：install-deb-ask.sh

--- install-deb-ask.sh
+++ install-deb-ask.sh
@@ -2,8 +2,6 @@
 # 
 
 
-INSTALL_LISTS=""
-
 install_for_file(){
 for deb in $(grep -Ev '^#|^$' "$1")
 do
@@ -11,36 +9,28 @@ do
 	read -n 1 yesno
 	echo ''
 	if [ "$yesno"x = "y"x ];then
-		INSTALL_LISTS="$INSTALL_LISTS $deb"
-		#sudo apt install "$deb"
+		sudo apt install "$deb"
 	else
 		echo "不安装$deb"
 	fi
 done
-
-apt install $INSTALL_LISTS
 }
 
 
 install_for_file "ubuntu软件包列表.txt"
 
-
-INSTALL_LISTS=""
-
 pip_install_for_file(){
-for pip in $(grep -Ev '^#|^$' "$1")
+for deb in $(grep -Ev '^#|^$' "$1")
 do
 	echo "安装$deb?[y/N]"
 	read -n 1 yesno
 	echo ''
 	if [ "$yesno"x = "y"x ];then
-		INSTALL_LISTS="$INSTALL_LISTS $pip"
-		#sudo pip3 install "$deb"
+		sudo pip3 install "$deb"
 	else
 		echo "不安装$deb"
 	fi
 done
-pip3 install $INSTALL_LISTS
 }
 
 pip_install_for_file "python第3方包.txt"
