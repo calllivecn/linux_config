5148e9c700923e2d618f04325b0cccb2e06ec161
calllivecn <c-all@qq.com>
2020-07-23 10:53:15 +0800

    bridge 相关

---
+++ skill/ipbridge.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# date 2020-07-23 10:13:11
+# author calllivecn <c-all@qq.com>
+
+
+HELP='Usage: ipbridge.sh <br_name> [slave1 [slave2...]]'
+
+
+if [ -n "$1" ];then
+	BR="$1"
+else
+	echo "需要bridge名"
+	exit 1
+fi
+
+ip link add "$BR" type bridge
+
+shift
+
+for slave in "$@"
+do
+	#echo "$slave"
+	ip link set "$slave" master "$BR" 
+done
+
+ip link set "$BR" up
