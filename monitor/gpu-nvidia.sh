3a87d84e1aa67e41893cb6e29bc1a9d0860250d6
calllivecn <c-all@qq.com>
2023-03-27 23:53:21 +0800

    保存下之前的prometheus+grafana监控配置 node_exporter pushgateway

---
+++ monitor/gpu-nvidia.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+# date 2022-01-09 06:26:25
+# author calllivecn <c-all@qq.com>
+
+. "$(dirname ${0})/libfuncs.sh"
+
+MY_JOB="gpu_nvidia"
+
+pushgateway(){
+	runtime_info=$(nvidia-smi |grep -A 1 "NVIDIA GeForce" |tail -n 1)
+	temp=$(echo "$runtime_info" | awk '{print $3}')
+	# 去掉温度符号
+	temp=${temp%C}
+
+	#功耗 
+	power_usage=$(echo "$runtime_info" | awk '{print $5}')
+	power_usage=${power_usage%W}
+
+	power_cap=$(echo "$runtime_info" | awk '{print $7}')
+	power_cap=${power_cap%W}
+	
+	memory_usage=$(echo "$runtime_info" | awk '{print $9}')
+	memory_usage=${memory_usage%MiB}
+	
+	memory_total=$(echo "$runtime_info" | awk '{print $11}')
+	memory_total=${memory_total%MiB}
+	
+	gpu_usage=$(echo "$runtime_info" | awk '{print $13}')
+	gpu_usage=${gpu_usage%\%}
+	
+context="
+gpu_nvidia_temp $temp
+gpu_nvidia_power_usage $power_usage
+gpu_nvidia_power_cap $power_cap
+gpu_nvidia_memory_usage $memory_usage
+gpu_nvidia_memory_total $memory_total
+gpu_nvidia_usage $gpu_usage
+"
+	echo "$context" | curl -s --data-binary @- "${PUSHGATEWAY}/metrics/job/${MY_JOB}/nodename/${NODENAME}" || true
+}
+
+while :
+do
+	pushgateway 
+	if [ $? -eq 0 ];then
+		#echo "$context"
+		:
+	else
+		echo "出问题了..."
+		break
+	fi
+	sleep 5
+done
---
+++ monitor/pushgateway-clear.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# date 2022-06-05 14:44:23
+# author calllivecn <c-all@qq.com>
+
+example(){
+	baseurl=localhost:9091
+	for uri in $(curl -sS $baseurl/api/v1/metrics | jq -r '
+	  .data[].push_time_seconds.metrics[0] |
+	  select((now - (.value | tonumber)) > 60) |
+	  (.labels as $labels | ["job", "instance"] | map(.+"/"+$labels[.]) | join("/"))
+	'); do
+	  curl -XDELETE $baseurl/metrics/$uri | exit
+	  echo curl -XDELETE $baseurl/metrics/$uri
+	done
+}
+
+
+URL="http://localhost:9091"
+
+check_expire_metric(){
+	metrics=$(curl $URL/api/v1/metrics)
+	
+	last_push_timestamp=$(echo "$metrics" |jsonfmt.py -d data[0].push_time_seconds.metrics[0].value)
+	
+	T=$(python3 -c "print(int(float($last_push_timestamp)))")
+	
+	CUR_T=$(date +%s)
+	interval=$[CUR_T - T]
+	
+	if [ $interval -ge 60 ];then
+		echo "delete job: gpu_nvidia nodename: zxdiy"
+		curl -X DELETE http://localhost:9091/metrics/job/gpu_nvidia/nodename/zxdiy
+	fi
+
+}
+
+while :
+do
+	check_expire_metric
+	sleep 60
+done
