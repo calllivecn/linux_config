7fc5cd45fe46c6e8f6c976ea2bc0e14cf06fe583
calllivecn <calllivecn@outlook.com>
2023-06-15 00:20:26 +0800

    save

--- config/pip-conf.sh
+++ config/pip-conf.sh
@@ -6,8 +6,15 @@
 # 配置系统源
 sudo pip3 config --global set global.index-url https://mirrors.aliyun.com/pypi/simple/
 
+# 添加额外的地址
+#sudo pip3 config --global set global.extra-index-url https://mirrors.aliyun.com/pypi/simple/
+
 # 这是配置当前用户的源
 #pip3 config set global.index-url https://mirrors.aliyun.com/pypi/simple/
+
+# 这是配置当前用户的源
+#pip3 config set global.extra-index-url https://mirrors.aliyun.com/pypi/simple/
+
 
 # 设置当前用户的 pip 缴存目录， 默认是 ~/.cache/pip
 #pip config set global.cache-dir /home/pip-cache
