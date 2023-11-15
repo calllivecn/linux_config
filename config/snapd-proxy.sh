1bf58473ac22a58c414b0bf7e0fd458c00983880
calllivecn <c-all@qq.com>
2023-11-15 20:41:57 +0800

    添加新的snapd 的代理方法

---
+++ config/snapd-proxy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# date 2023-11-15 20:39:54
+# author calllivecn <c-all@qq.com>
+
+
+sudo snap set system proxy.http="http://10.1.3.254:9999"
+sudo snap set system proxy.https="http://10.1.3.254:9999"
+
+
