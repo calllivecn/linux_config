588953c5f83bd78e9a4b731c895ddec07d34a800
calllivecn <calllivecn@outlook.com>
2024-02-02 08:42:08 +0800

    添加vbox压缩硬盘工具

---
+++ skill/vbox-comp-disk.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# coding=utf-8
+# date 2024-02-02 08:41:38
+# author calllivecn <c-all@qq.com>
+
+
+import os
+
+os.chdir("""c:\\""")
+
+
+BUF = bytes((1<<20))
+
+
+try:
+    with open("empty", "wb") as f:
+        f.seek(0, os.SEEK_END)
+        c = 0
+        while True:
+            f.write(BUF)
+            f.flush()
+            c += 1
+            print("已写入{}M".format(c))
+
+except OSError:
+    print("磁盘已满。")
+    #os.remove("empty")
+
+
+
+
+
